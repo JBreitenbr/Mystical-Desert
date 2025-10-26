462fff14de72ca6dd28a51064df6f314e8217ea1
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2025-10-26 20:50:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function multiplyNode(node, count, deep) {
         node.parentNode.insertBefore(copy, node);
     }
 }
-for(let i=1;i<4;i++){
+for(let i=1;i<10;i++){
 if(window.innerWidth<400){
-multiplyNode(document.querySelector(`.pic${i}`), 12, true);
+multiplyNode(document.querySelector(`.pic${i}`), 11, true);
 }
-else { multiplyNode(document.querySelector(`.pic${i}`),Math.floor(window.innerWidth/50),true);
+else { multiplyNode(document.querySelector(`.pic${i}`),Math.floor(window.innerWidth/50)-1,true);
      };
   };
